81ac6e65d86cee24f258a20a9d138193ebf65335
darks <mail@stephanl.de>
2022-01-28 17:05:44 +0100

    some formatting and correction of import

--- autoassist.py
+++ autoassist.py
@@ -1,17 +1,21 @@
-from PyTado.PyTado.interface import Tado
+from PyTado.interface import Tado
 from datetime import datetime
-import time, threading
+import time
+import threading
 
 log = "tado-autoassist.log"
-loginlist = [('user1@example.com', 'PASSWORD1'), ('user2@example.com', 'PASSWORD2')]
+loginlist = [('user1@example.com', 'PASSWORD1'),
+             ('user2@example.com', 'PASSWORD2')]
 userlist = []
+
 
 def login(user, secret):
     userdata = []
     userdata.append(Tado(user, secret))                     # user
     userdata.append(userdata[0].getZones())                 # zones
-    userdata.append(userdata[0].getHomeState()["presence"]) # homestate
+    userdata.append(userdata[0].getHomeState()["presence"])  # homestate
     return userdata
+
 
 def autoassist(userdata):
     t = userdata[0]
@@ -20,12 +24,14 @@ def autoassist(userdata):
     # Open Window Detection
     for zone in zones:
         try:
-            iswindowopenzone = t.getOpenWindowDetected(zone["id"])["openWindowDetected"]
+            iswindowopenzone = t.getOpenWindowDetected(
+                zone["id"])["openWindowDetected"]
         except Exception as e:
             writeToLog("An Error Occurred: " + str(e))
             iswindowopenzone = False
         if iswindowopenzone:
-            writeToLog("Open Window in '" + zone["name"] + "' detected: Switching to open-window-mode.")
+            writeToLog("Open Window in '"
+                       + zone["name"] + "' detected: Switching to open-window-mode.")
             try:
                 t.setOpenWindow(zone["id"])
             except Exception as e:
@@ -43,10 +49,11 @@ def autoassist(userdata):
         writeToLog("An Error Occurred: " + str(e))
     for device in mobileDevices:
         if device["settings"]["geoTrackingEnabled"]:
-            print("HomeState: " + userdata[2] + ", Location at Home: " + str(device["location"]["atHome"]))
-            if device["location"]["atHome"]: # Person zu Hause
+            print("HomeState: "
+                  + userdata[2] + ", Location at Home: " + str(device["location"]["atHome"]))
+            if device["location"]["atHome"]:  # someone is home
                 someonehome = True
-                if (userdata[2] == "AWAY"): # System sagt niemand zu Hause
+                if (userdata[2] == "AWAY"):  # nobody home
                     writeToLog("Presence detected: Switching to home-mode.")
                     try:
                         t.setHome()
@@ -64,12 +71,14 @@ def autoassist(userdata):
             userdata[2] = "AWAY"
     return userdata
 
+
 def syncHomeState():
     for user in userlist:
         try:
             user[2] = user[0].getHomeState()
         except Exception as e:
             writeToLog("An Error Occurred: " + str(e))
+
 
 def writeToLog(msg):
     filelength = 10
@@ -79,31 +88,32 @@ def writeToLog(msg):
     with open(log, "w") as outfile:
         outfile.write("[" + str(datetime.now()) + "] " + msg + "\n")
         print("[" + str(datetime.now()) + "] " + msg)
-        for index,line in enumerate(lines):
+        for index, line in enumerate(lines):
             if index < (filelength-1):
                 outfile.write(line)
 
+
 def action():
     for user in userlist:
-        user = autoassist(user);
+        user = autoassist(user)
 
-class setInterval :
-    def __init__(self,interval,action) :
-        self.interval=interval
-        self.action=action
-        self.stopEvent=threading.Event()
-        thread=threading.Thread(target=self.__setInterval)
+
+class setInterval:
+    def __init__(self, interval, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = threading.Event()
+        thread = threading.Thread(target=self.__setInterval)
         thread.start()
 
-    def __setInterval(self) :
-        nextTime=time.time()+self.interval
-        while not self.stopEvent.wait(nextTime-time.time()) :
-            nextTime+=self.interval
+    def __setInterval(self):
+        nextTime = time.time()+self.interval
+        while not self.stopEvent.wait(nextTime-time.time()):
+            nextTime += self.interval
             self.action()
 
-    def cancel(self) :
+    def cancel(self):
         self.stopEvent.set()
-
 
 
 for i in range(len(loginlist)):
@@ -115,9 +125,8 @@ for i in range(len(loginlist)):
         exit()
 
 
-
 # start action every 10s
-inter=setInterval(10,action)
+inter = setInterval(10, action)
 # synchronize the HomeState every min
-inter2=setInterval(60,syncHomeState)
+inter2 = setInterval(60, syncHomeState)
 writeToLog('Py-Auto-Assist started!')
